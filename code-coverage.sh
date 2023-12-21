caec43487af219ef521835b13be6ec9861c2b93e
Thomas Luijken <tluijken@outlook.com>
2023-12-21 15:55:31 +0100

    Added convenient shell script to open code coverage page

---
+++ code-coverage.sh
@@ -0,0 +1,5 @@
+CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test
+grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html
+rm *.profraw
+# open the coverage report in the browser
+xdg-open target/coverage/html/index.html
