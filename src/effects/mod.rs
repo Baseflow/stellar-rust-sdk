c46fb18a551377dcc33dd2f5d1778ad8fe287321
leonard <leonard@stellar-sdk.fedora>
2024-02-13 09:23:30 +0100

    effects for account
    effects for account tests

--- src/effects/mod.rs
+++ src/effects/mod.rs
@@ -1,11 +1,23 @@
 pub mod all_effects_request;
-
 pub mod all_effects_response;
-
+pub mod effects_for_account_request;
+pub mod effects_for_account_response;
 
 static EFFECTS_PATH: &str = "effects";
 
 pub mod prelude {
     pub use super::all_effects_request::*;
     pub use super::all_effects_response::*;
+    pub use super::effects_for_account_request::*;
+    pub use super::effects_for_account_response::*;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dummy_test() {
+        assert_eq!(EFFECTS_PATH, "effects");
+    }
 }
