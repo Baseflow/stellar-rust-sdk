2e4a4abe833b0818469c4a079ba299f08d639e86
LeonardTibben <leonard@baseflow.com>
2024-03-11 16:43:07 +0100

    test get all single liquidity pool

--- src/liquidity_pools/mod.rs
+++ src/liquidity_pools/mod.rs
@@ -102,6 +102,23 @@ async fn test_get_all_liquidity_pools() {
 
 #[tokio::test]
 async fn test_get_single_liquidity_pool() {
+    const LIQUIDITY_POOL_ID: &str =
+        "01c58ab8fb283c8b083a26bf2fe06b7b6c6304c13f9d29d956cdf15a48bea72d";
+    const LIQUIDITY_POOL_PAGING_TOKEN: &str =
+        "01c58ab8fb283c8b083a26bf2fe06b7b6c6304c13f9d29d956cdf15a48bea72d";
+    const LIQUIDITY_POOL_FEE_BP: i64 = 30;
+    const LIQUIDITY_POOL_TYPE: &str = "constant_product";
+    const LIQUIDITY_POOL_TOTAL_TRUSTLINES: &str = "1";
+    const LIQUIDITY_POOL_TOTAL_SHARES: &str = "150.0000000";
+    const LIQUIDITY_POOL_RESERVE_ASSET_0: &str =
+        "SDK:GB7IIVQLLJ3AY3DWSCACVJBZL7FFKDK4D3PMHPBBAIHPVVB3BZYUG5UN";
+    const LIQUIDITY_POOL_RESERVE_AMOUNT_0: &str = "160.0000000";
+    const LIQUIDITY_POOL_RESERVE_ASSET_1: &str =
+        "PHPSTAR:GAJHVDRVJHC2ORAERVVQYWADMEJLUX6H2SZ5MMEPI7BUHJYSTTFRZW7W";
+    const LIQUIDITY_POOL_RESERVE_AMOUNT_1: &str = "140.6513722";
+    const LIQUIDITY_POOL_LAST_MODIFIED_LEDGER: i64 = 249832;
+    const LIQUIDITY_POOL_LAST_MODIFIED_TIME: &str = "2024-02-21T22:18:27Z";
+
     let horizon_client =
         HorizonClient::new("https://horizon-testnet.stellar.org".to_string()).unwrap();
 
@@ -116,4 +133,52 @@ async fn test_get_single_liquidity_pool() {
         .await;
 
     assert!(single_liquidity_pool_response.clone().is_ok());
+
+    let single_liquidity_pool_response = single_liquidity_pool_response.unwrap();
+
+    assert_eq!(single_liquidity_pool_response.id(), LIQUIDITY_POOL_ID);
+    assert_eq!(
+        single_liquidity_pool_response.paging_token(),
+        LIQUIDITY_POOL_PAGING_TOKEN
+    );
+    assert_eq!(
+        single_liquidity_pool_response.fee_bp(),
+        &LIQUIDITY_POOL_FEE_BP
+    );
+    assert_eq!(
+        single_liquidity_pool_response.type_field(),
+        LIQUIDITY_POOL_TYPE
+    );
+    assert_eq!(
+        single_liquidity_pool_response.total_trustlines(),
+        LIQUIDITY_POOL_TOTAL_TRUSTLINES
+    );
+    assert_eq!(
+        single_liquidity_pool_response.total_shares(),
+        LIQUIDITY_POOL_TOTAL_SHARES
+    );
+    assert_eq!(
+        single_liquidity_pool_response.reserves()[0].asset(),
+        LIQUIDITY_POOL_RESERVE_ASSET_0
+    );
+    assert_eq!(
+        single_liquidity_pool_response.reserves()[0].amount(),
+        LIQUIDITY_POOL_RESERVE_AMOUNT_0
+    );
+    assert_eq!(
+        single_liquidity_pool_response.reserves()[1].asset(),
+        LIQUIDITY_POOL_RESERVE_ASSET_1
+    );
+    assert_eq!(
+        single_liquidity_pool_response.reserves()[1].amount(),
+        LIQUIDITY_POOL_RESERVE_AMOUNT_1
+    );
+    assert_eq!(
+        single_liquidity_pool_response.last_modified_ledger(),
+        &LIQUIDITY_POOL_LAST_MODIFIED_LEDGER
+    );
+    assert_eq!(
+        single_liquidity_pool_response.last_modified_time(),
+        LIQUIDITY_POOL_LAST_MODIFIED_TIME
+    );
 }
