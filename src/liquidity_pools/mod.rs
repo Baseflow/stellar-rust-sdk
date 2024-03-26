fdfae43e5114b5a54d6ebb194ff5d19a83456f1c
LeonardTibben <leonard@baseflow.com>
2024-03-26 11:33:58 +0100

    added another test to test all liquidity pools test

--- src/liquidity_pools/mod.rs
+++ src/liquidity_pools/mod.rs
@@ -42,10 +42,25 @@ async fn test_get_all_liquidity_pools() {
         "NOODLE:GARPXPHGABTN52WPJ2QZQBY7TSXMK7PXLKOD6FSPA2TEVSJDWIDRSHPO";
     const RSP_2_LIQUIDITY_POOL_RESERVE_AMOUNT_1: &str = "0.0000000";
 
+    const RSP_3_LIQUIDITY_POOL_ID: &str =
+        "02ea143d8ac56fa6e936c3558edfce6c64b7a223827a006805b279104bac2851";
+    const RSP_3_LIQUIDITY_POOL_PAGING_TOKEN: &str =
+        "02ea143d8ac56fa6e936c3558edfce6c64b7a223827a006805b279104bac2851";
+    const RSP_3_LIQUIDITY_POOL_FEE_BP: i64 = 30;
+    const RSP_3_LIQUIDITY_POOL_TYPE: &str = "constant_product";
+    const RSP_3_LIQUIDITY_POOL_TOTAL_TRUSTLINES: &str = "2";
+    const RSP_3_LIQUIDITY_POOL_TOTAL_SHARES: &str = "5196.1524225";
+    const RSP_3_LIQUIDITY_POOL_RESERVE_ASSET_0: &str =
+        "LPA7:GDTCZZNMT74SEGPDQL3IQJDJ54MXQJHSNQ3HNNJJIJLIVG2LOSDLG2OZ";
+    const RSP_3_LIQUIDITY_POOL_RESERVE_AMOUNT_0: &str = "3000.0000000";
+    const RSP_3_LIQUIDITY_POOL_RESERVE_ASSET_1: &str =
+        "LPB7:GDTBT5GAABYAENDWYXB2IAHYXMWDGK2ZG5H4ZDQL32DR7KS6L7NGC5I3";
+    const RSP_3_LIQUIDITY_POOL_RESERVE_AMOUNT_1: &str = "9000.0000000";
+
     let horizon_client =
         HorizonClient::new("https://horizon-testnet.stellar.org".to_string()).unwrap();
 
-    let all_liquidity_pools_request = AllLiquidityPoolsRequest::new()
+    let all_liquidity_pools_request_1 = AllLiquidityPoolsRequest::new()
         .add_native_reserve()
         .add_alphanumeric4_reserve(
             "USDC".to_string(),
@@ -53,46 +68,46 @@ async fn test_get_all_liquidity_pools() {
         )
         .set_limit(2);
 
-    let all_liquidity_pools_response = horizon_client
-        .get_all_liquidity_pools(&all_liquidity_pools_request)
+    let all_liquidity_pools_response_1 = horizon_client
+        .get_all_liquidity_pools(&all_liquidity_pools_request_1)
         .await;
 
-    assert!(all_liquidity_pools_response.clone().is_ok());
+    assert!(all_liquidity_pools_response_1.clone().is_ok());
 
-    let binding = all_liquidity_pools_response.unwrap();
-    let all_liquidity_pools_response = &binding.embedded().records()[0];
+    let binding = all_liquidity_pools_response_1.unwrap();
+    let all_liquidity_pools_response_2 = &binding.embedded().records()[0];
 
-    assert_eq!(all_liquidity_pools_response.id(), RSP_1_LIQUIDITY_POOL_ID);
+    assert_eq!(all_liquidity_pools_response_2.id(), RSP_1_LIQUIDITY_POOL_ID);
     assert_eq!(
-        all_liquidity_pools_response.paging_token(),
+        all_liquidity_pools_response_2.paging_token(),
         RSP_1_LIQUIDITY_POOL_PAGING_TOKEN
     );
     assert_eq!(
-        all_liquidity_pools_response.fee_bp(),
+        all_liquidity_pools_response_2.fee_bp(),
         &RSP_1_LIQUIDITY_POOL_FEE_BP
     );
     assert_eq!(
-        all_liquidity_pools_response.type_field(),
+        all_liquidity_pools_response_2.type_field(),
         RSP_1_LIQUIDITY_POOL_TYPE
     );
     assert_eq!(
-        all_liquidity_pools_response.total_trustlines(),
+        all_liquidity_pools_response_2.total_trustlines(),
         RSP_1_LIQUIDITY_POOL_TOTAL_TRUSTLINES
     );
     assert_eq!(
-        all_liquidity_pools_response.reserves()[0].asset(),
+        all_liquidity_pools_response_2.reserves()[0].asset(),
         RSP_1_LIQUIDITY_POOL_RESERVE_ASSET_0
     );
     assert_eq!(
-        all_liquidity_pools_response.reserves()[0].amount(),
+        all_liquidity_pools_response_2.reserves()[0].amount(),
         RSP_1_LIQUIDITY_POOL_RESERVE_AMOUNT_0
     );
     assert_eq!(
-        all_liquidity_pools_response.reserves()[1].asset(),
+        all_liquidity_pools_response_2.reserves()[1].asset(),
         RSP_1_LIQUIDITY_POOL_RESERVE_ASSET_1
     );
     assert_eq!(
-        all_liquidity_pools_response.reserves()[1].amount(),
+        all_liquidity_pools_response_2.reserves()[1].amount(),
         RSP_1_LIQUIDITY_POOL_RESERVE_AMOUNT_1
     );
 
@@ -150,7 +165,60 @@ async fn test_get_all_liquidity_pools() {
         all_liquidity_pools_response_2.reserves()[1].amount(),
         RSP_2_LIQUIDITY_POOL_RESERVE_AMOUNT_1
     );
-    
+
+    let all_liquidity_pools_request_3 = AllLiquidityPoolsRequest::new()
+        .add_alphanumeric4_reserve(
+            "LPA7".to_string(),
+            "GDTCZZNMT74SEGPDQL3IQJDJ54MXQJHSNQ3HNNJJIJLIVG2LOSDLG2OZ".to_string(),
+        )
+        .set_limit(2);
+
+    let all_liquidity_pools_response_3 = horizon_client
+        .get_all_liquidity_pools(&all_liquidity_pools_request_3)
+        .await;
+
+    assert!(all_liquidity_pools_response_3.clone().is_ok());
+
+    let binding = all_liquidity_pools_response_3.unwrap();
+    let all_liquidity_pools_response_3 = &binding.embedded().records()[0];
+
+    assert_eq!(all_liquidity_pools_response_3.id(), RSP_3_LIQUIDITY_POOL_ID);
+    assert_eq!(
+        all_liquidity_pools_response_3.paging_token(),
+        RSP_3_LIQUIDITY_POOL_PAGING_TOKEN
+    );
+    assert_eq!(
+        all_liquidity_pools_response_3.fee_bp(),
+        &RSP_3_LIQUIDITY_POOL_FEE_BP
+    );
+    assert_eq!(
+        all_liquidity_pools_response_3.type_field(),
+        RSP_3_LIQUIDITY_POOL_TYPE
+    );
+    assert_eq!(
+        all_liquidity_pools_response_3.total_trustlines(),
+        RSP_3_LIQUIDITY_POOL_TOTAL_TRUSTLINES
+    );
+    assert_eq!(
+        all_liquidity_pools_response_3.total_shares(),
+        RSP_3_LIQUIDITY_POOL_TOTAL_SHARES
+    );
+    assert_eq!(
+        all_liquidity_pools_response_3.reserves()[0].asset(),
+        RSP_3_LIQUIDITY_POOL_RESERVE_ASSET_0
+    );
+    assert_eq!(
+        all_liquidity_pools_response_3.reserves()[0].amount(),
+        RSP_3_LIQUIDITY_POOL_RESERVE_AMOUNT_0
+    );
+    assert_eq!(
+        all_liquidity_pools_response_3.reserves()[1].asset(),
+        RSP_3_LIQUIDITY_POOL_RESERVE_ASSET_1
+    );
+    assert_eq!(
+        all_liquidity_pools_response_3.reserves()[1].amount(),
+        RSP_3_LIQUIDITY_POOL_RESERVE_AMOUNT_1
+    );
 }
 
 #[tokio::test]
