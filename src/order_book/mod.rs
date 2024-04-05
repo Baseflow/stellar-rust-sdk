66caf08dacbe1d779ca2caea51a6194d4112ec08
LeonardTibben <leonard@baseflow.com>
2024-04-05 15:26:44 +0200

    documentation

--- src/order_book/mod.rs
+++ src/order_book/mod.rs
@@ -3,7 +3,7 @@ pub mod response;
 
 static ORDER_BOOK_PATH: &str = "order_book";
 
-mod prelude {
+pub mod prelude {
     pub use super::details_request::*;
     pub use super::response::*;
 }
@@ -16,6 +16,22 @@ pub mod tests {
 
     #[tokio::test]
     async fn get_order_bookdetails() {
+        const BIDS_N: &i64 = &1000;
+        const BIDS_D: &i64 = &87;
+        const BIDS_PRICE: &str = "11.4942529";
+        const BIDS_AMOUNT: &str = "2574773.7512805";
+
+        const ASKS_N: &i64 = &2299;
+        const ASKS_D: &i64 = &200; 
+        const ASKS_PRICE: &str = "11.4950000";
+        const ASKS_AMOUNT: &str = "221878.4738782";
+
+        const BASE_ASSET_TYPE: &str = "credit_alphanum4";
+        const BASE_ASSET_CODE: &str = "USDC";
+        const BASE_ASSET_ISSUER: &str = "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5";
+
+        const COUNTER_ASSET_TYPE: &str = "native";
+
         let horizon_client =
             horizon_client::HorizonClient::new("https://horizon-testnet.stellar.org".to_string())
                 .unwrap();
@@ -32,8 +48,24 @@ pub mod tests {
 
         let details_response = horizon_client.get_order_book_details(&details_request).await;
         
-        details_response.unwrap();
+        assert!(details_response.is_ok());
 
-        // assert!(details_response.is_ok());
+        let binding = details_response.unwrap();
+
+        assert_eq!(binding.bids()[0].price_r().n(), BIDS_N);
+        assert_eq!(binding.bids()[0].price_r().d(), BIDS_D);
+        assert_eq!(binding.bids()[0].price(), BIDS_PRICE);
+        assert_eq!(binding.bids()[0].amount(), BIDS_AMOUNT);
+
+        assert_eq!(binding.asks()[0].price_r().n(), ASKS_N);
+        assert_eq!(binding.asks()[0].price_r().d(), ASKS_D);
+        assert_eq!(binding.asks()[0].price(), ASKS_PRICE);
+        assert_eq!(binding.asks()[0].amount(), ASKS_AMOUNT);
+
+        assert_eq!(binding.base().asset_type().as_deref(), Some(BASE_ASSET_TYPE));
+        assert_eq!(binding.base().asset_code().as_deref(), Some(BASE_ASSET_CODE));
+        assert_eq!(binding.base().asset_issuer().as_deref(), Some(BASE_ASSET_ISSUER));
+
+        assert_eq!(binding.counter().asset_type().as_deref(), Some(COUNTER_ASSET_TYPE));
     }
 }
--- src/order_book/response.rs
+++ src/order_book/response.rs
@@ -1,17 +1,29 @@
+use derive_getters::Getters;
 use serde::{Deserialize, Serialize};
 
 use crate::models::Response;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Represents the response from the Horizon server when querying for the details of an order book.
+/// 
+/// This struct represents the response from the Horizon server when querying for the details of an order book.
+/// It includes the bids, asks, base, and counter fields.
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 pub struct DetailsResponse {
+    /// The bids in the order book.
     pub bids: Vec<Bid>,
+    /// The asks in the order book.
     pub asks: Vec<Ask>,
+    /// The base asset of the order book.
     pub base: Base,
+    /// The counter asset of the order book.
     pub counter: Counter,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Represents a bid in the order book.
+/// 
+/// This struct represents a bid in the order book. It includes the price_r, price, and amount fields.
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 pub struct Bid {
     #[serde(rename = "price_r")]
@@ -20,14 +32,20 @@ pub struct Bid {
     pub amount: String,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Represents the price_r field in the order book.
+/// 
+/// This struct represents the price_r field in the order book. It includes the n and d fields.
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 pub struct PriceR {
     pub n: i64,
     pub d: i64,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Represents an ask in the order book.
+/// 
+/// This struct represents an ask in the order book. It includes the price_r, price, and amount fields.
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 pub struct Ask {
     #[serde(rename = "price_r")]
@@ -36,7 +54,10 @@ pub struct Ask {
     pub amount: String,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Represents the base asset of the order book.
+/// 
+/// This struct represents the base asset of the order book. It includes the asset_type, asset_code, and asset_issuer fields.
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 pub struct Base {
     #[serde(rename = "asset_type")]
@@ -47,7 +68,10 @@ pub struct Base {
     pub asset_issuer: Option<String>,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Represents the counter asset of the order book.
+/// 
+/// This struct represents the counter asset of the order book. It includes the asset_type, asset_code, and asset_issuer fields.
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 pub struct Counter {
     #[serde(rename = "asset_type")]
