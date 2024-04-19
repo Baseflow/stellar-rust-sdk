73434b591bd993631a28b73b9401f984612200bd
Thomas Luijken <thomas@luijken.dev>
2024-04-19 13:00:40 +0200

    Fixed tests

--- stellar_rust_sdk/src/order_book/response.rs
+++ stellar_rust_sdk/src/order_book/response.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 use crate::models::Response;
 
 /// Represents the response from the Horizon server when querying for the details of an order book.
-/// 
+///
 /// This struct represents the response from the Horizon server when querying for the details of an order book.
 /// It includes the bids, asks, base, and counter fields.
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Getters)]
@@ -26,7 +26,7 @@ pub struct DetailsResponse {
 pub struct Bid {
     #[serde(rename = "price_r")]
     /// A precise representation of the bid price of the asset pair.
-    pub price_r: PriceR,
+    pub price_ratio: PriceR,
     /// The bid price of the base asset denominated in the counter asset. A number representing the decimal form of price_r.
     pub price: String,
     /// The amount of counter asset that the account making this offer is willing to buy at this price.
@@ -37,10 +37,12 @@ pub struct Bid {
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize, Getters)]
 #[serde(rename_all = "camelCase")]
 pub struct PriceR {
-    /// The numerator
-    pub n: i64,
-    /// The denominator
-    pub d: i64,
+    /// The numenator.
+    #[serde(rename = "n")]
+    numenator: u32,
+    /// The denominator.
+    #[serde(rename = "d")]
+    denominator: u32,
 }
 
 /// The prices and amounts for the sellside of the asset pair.
@@ -49,7 +51,7 @@ pub struct PriceR {
 pub struct Ask {
     #[serde(rename = "price_r")]
     /// A precise representation of the ask price of the asset pair.
-    pub price_r: PriceR,
+    pub price_ratio: PriceR,
     /// The ask price of the base asset denominated in the counter asset. A number representing the decimal form of
     pub price: String,
     /// The amount of counter asset that the account making this offer is willing to sell at this price.
@@ -88,9 +90,9 @@ pub struct Counter {
 
 impl Response for DetailsResponse {
     fn from_json(json: String) -> Result<Self, String> {
-
         let operation_record = serde_json::from_str(&json).map_err(|e| e.to_string())?;
 
         Ok(operation_record)
     }
 }
+
