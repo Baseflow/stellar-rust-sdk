aafc8ec1b42dc3ee093e0f4a5b6042cae1abee2a
Kevin Pease <kevin@baseflow.com>
2024-05-22 14:13:20 +0200

    Add documentation and fix minor details

--- stellar_rust_sdk/src/transactions/all_transactions_request.rs
+++ stellar_rust_sdk/src/transactions/all_transactions_request.rs
@@ -2,7 +2,35 @@ use crate::{models::*, BuildQueryParametersExt};
 use stellar_rust_sdk_derive::Pagination;
 use crate::Paginatable;
 
-// TODO: Documentation
+/// Represents a request to list all transactions from the Stellar Horizon API.
+///
+/// This structure is used to construct a query to retrieve a comprehensive list of transactions, which
+/// can be filtered by setting `include_failed`. It adheres to the structure and parameters required
+/// by the Horizon API for retrieving a
+/// <a href="https://developers.stellar.org/network/horizon/api-reference/resources/list-all-transactions">list of all transactions</a>.
+///
+/// # Usage
+///
+/// Create an instance of this struct and set the desired query parameters to filter the list of transactions.
+/// Pass this request object to the [`HorizonClient::get_all_transactions`](crate::horizon_client::HorizonClient::get_all_transactions)
+/// method to fetch the corresponding data from the Horizon API.
+///
+/// # Example
+/// ```
+/// use stellar_rs::transactions::all_transactions_request::AllTransactionsRequest;
+/// use stellar_rs::models::{Order};
+/// use stellar_rust_sdk_derive::Pagination;
+/// use stellar_rs::Paginatable;
+///
+/// let request = AllTransactionsRequest::new()
+///     .set_include_failed(true).unwrap() // Optional flag to include failed transactions
+///     .set_cursor(123).unwrap() // Optional cursor for pagination
+///     .set_limit(100).unwrap() // Optional limit for response records
+///     .set_order(Order::Desc); // Optional order of records
+///
+/// // Use with HorizonClient::get_all_transactions
+/// ```
+///
 #[derive(Default, Pagination)]
 pub struct AllTransactionsRequest {
     // Indicates whether or not to include failed operations in the response.
@@ -29,7 +57,6 @@ impl Request for AllTransactionsRequest {
         .build_query_parameters()
     }
 
-    // TODO: Documentation
     fn build_url(&self, base_url: &str) -> String {
         format!(
             "{}/{}{}",
@@ -46,7 +73,10 @@ impl AllTransactionsRequest {
         AllTransactionsRequest::default()
     }
 
-    // TODO: Documentation
+    /// Specifies whether to include failed operations in the response.
+    ///
+    /// # Arguments
+    /// * `include_failed` (bool) - when set to `true`, failed operations will be included.
     pub fn set_include_failed(self, include_failed: bool) -> Result<AllTransactionsRequest, String> {
         Ok(AllTransactionsRequest {
             include_failed: Some(include_failed),
