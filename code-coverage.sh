e6c0e434f16382b1b86289ffa6d05c30f9bc3501
Kevin Pease <kevin@baseflow.com>
2024-05-24 13:37:59 +0200

    Fix path for xdg-open, add ./ for paths

--- code-coverage.sh
+++ code-coverage.sh
@@ -1,5 +1,5 @@
-CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test --manifest-path=stellar_rust_sdk/Cargo.toml
-grcov . --binary-path stellar_rust_sdk/target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o stellar_rust_sdk/target/coverage/html
-rm stellar_rust_sdk/*.profraw
+CARGO_INCREMENTAL=0 RUSTFLAGS='-Cinstrument-coverage' LLVM_PROFILE_FILE='cargo-test-%p-%m.profraw' cargo test --manifest-path=./stellar_rust_sdk/Cargo.toml
+grcov . --binary-path ./stellar_rust_sdk/target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o ./stellar_rust_sdk/target/coverage/html
+rm ./stellar_rust_sdk/*.profraw
 # open the coverage report in the browser
-xdg-open target/coverage/html/index.html
+xdg-open ./stellar_rust_sdk/target/coverage/html/index.html
