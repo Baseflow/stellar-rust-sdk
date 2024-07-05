fedc72ab275f97ba5c2a4f3964c8cdcb530df677
leonard <leonard@baseflow.com>
2024-07-05 14:36:26 +0200

    fixed orderbook tests

--- stellar_rust_sdk/src/order_book/mod.rs
+++ stellar_rust_sdk/src/order_book/mod.rs
@@ -14,32 +14,32 @@ pub mod tests {
 
     #[tokio::test]
     async fn get_order_bookdetails() {
-        const BIDS_N: &u32 = &1000;
-        const BIDS_D: &u32 = &87;
-        const BIDS_PRICE: &str = "11.4942529";
-        const BIDS_AMOUNT: &str = "2556626.8467920";
+        const BIDS_N: &u32 = &6;
+        const BIDS_D: &u32 = &1;
+        const BIDS_PRICE: &str = "6.0000000";
+        const BIDS_AMOUNT: &str = "14.2857142";
 
-        const ASKS_N: &u32 = &2299;
-        const ASKS_D: &u32 = &200;
-        const ASKS_PRICE: &str = "11.4950000";
-        const ASKS_AMOUNT: &str = "162468.5993642";
+        const ASKS_N: &u32 = &7;
+        const ASKS_D: &u32 = &1;
+        const ASKS_PRICE: &str = "7.0000000";
+        const ASKS_AMOUNT: &str = "99.9999996";
 
-        const BASE_ASSET_TYPE: &str = "credit_alphanum4";
-        const BASE_ASSET_CODE: &str = "USDC";
-        const BASE_ASSET_ISSUER: &str = "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5";
+        const BASE_ASSET_TYPE: &str = "native";
+        const BASE_ASSET_CODE: &str = "IOM";
+        const BASE_ASSET_ISSUER: &str = "GCDE6MVFIOYF7YZCSVA6V7MDCFTNWMIOF5PQU3DWPH27AHNX4ERY6AKS";
 
-        const COUNTER_ASSET_TYPE: &str = "native";
+        const COUNTER_ASSET_TYPE: &str = "credit_alphanum4";
 
         let horizon_client =
             horizon_client::HorizonClient::new("https://horizon-testnet.stellar.org".to_string())
                 .unwrap();
 
         let details_request = DetailsRequest::new()
-            .set_buying_asset(AssetType::Native)
+            .set_selling_asset(AssetType::Native)
             .unwrap()
-            .set_selling_asset(AssetType::Alphanumeric4(Asset {
-                asset_code: "USDC".to_string(),
-                asset_issuer: "GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5"
+            .set_buying_asset(AssetType::Alphanumeric4(Asset {
+                asset_code: "IOM".to_string(),
+                asset_issuer: "GCDE6MVFIOYF7YZCSVA6V7MDCFTNWMIOF5PQU3DWPH27AHNX4ERY6AKS"
                     .to_string(),
             }))
             .unwrap();
@@ -58,14 +58,12 @@ pub mod tests {
 
         // The amount changes all the time
         assert_ne!(binding.bids()[0].amount(), "0");
-        //assert_eq!(binding.bids()[0].amount(), BIDS_AMOUNT);
 
         assert_eq!(binding.asks()[0].price_ratio().numenator(), ASKS_N);
         assert_eq!(binding.asks()[0].price_ratio().denominator(), ASKS_D);
         assert_eq!(binding.asks()[0].price(), ASKS_PRICE);
         // The amount changes all the time
         assert_ne!(binding.asks()[0].amount(), "0");
-        //assert_eq!(binding.asks()[0].amount(), ASKS_AMOUNT);
 
         assert_eq!(
             binding.base().asset_type().as_deref(),
@@ -73,16 +71,26 @@ pub mod tests {
         );
         assert_eq!(
             binding.base().asset_code().as_deref(),
-            Some(BASE_ASSET_CODE)
+            None
         );
         assert_eq!(
             binding.base().asset_issuer().as_deref(),
-            Some(BASE_ASSET_ISSUER)
+            None
         );
 
         assert_eq!(
             binding.counter().asset_type().as_deref(),
             Some(COUNTER_ASSET_TYPE)
+        );
+
+        assert_eq!(
+            binding.counter().asset_code().as_deref(),
+            Some(BASE_ASSET_CODE)
+        );
+
+        assert_eq!(
+            binding.counter().asset_issuer().as_deref(),
+            Some(BASE_ASSET_ISSUER)
         );
     }
 }
