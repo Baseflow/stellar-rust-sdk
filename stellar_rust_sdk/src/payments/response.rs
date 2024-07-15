c3d19c41e7ec10a4dbaabbc81a6878e5a5d59ecc
leonard <leonard@baseflow.com>
2024-07-15 10:11:50 +0200

    payments endpoint wip

---
+++ stellar_rust_sdk/src/payments/response.rs
@@ -0,0 +1,41 @@
+use derive_getters::Getters;
+use serde::{Deserialize, Serialize};
+use crate::liquidity_pools::prelude::LiquidityPool;
+use crate::models::prelude::{Embedded, ResponseLinks};
+
+/// Represents the response from the Horizon server when querying for all payments.
+///
+/// This struct represents the response from the Horizon server when querying for all payments.
+/// It includes the links to the current, next, and previous pages of the response, as well as the
+/// embedded records of payments.
+#[derive(Default, Debug, Clone, Serialize, Deserialize, Getters)]
+#[serde(rename_all = "camelCase")]
+pub struct AllPaymentsResponse {
+    /// The links to the current, next, and previous pages of the response.
+    #[serde(rename = "_links")]
+    pub links: ResponseLinks,
+    /// The embedded records of liquidity pools.
+    #[serde(rename = "_embedded")]
+    pub embedded: Embedded<LiquidityPool>,
+}
+
+/// Represents the payment record in the Horizon API response.
+///
+/// This struct encapsulates detailed information about a single payment, including its ID, paging token,
+/// transaction success status, source account, type, creation date, transaction hash, starting balance,
+/// funder, and account.
+#[derive(Default, Debug, Clone, Serialize, Deserialize, Getters)]
+#[serde(rename_all = "camelCase")]
+pub struct Payment {
+    pub id: String,
+    pub paging_token: String,
+    pub transaction_successful: bool,
+    pub source_account: String,
+    pub type_field: String,
+    pub type_i: i64,
+    pub created_at: String,
+    pub transaction_hash: String,
+    pub starting_balance: String,
+    pub funder: String,
+    pub account: String,
+}
