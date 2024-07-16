507b448fb07219effc7becd833baf8b4d62b8d20
Kevin Pease <kevin@baseflow.com>
2024-07-16 13:15:58 +0200

    Initial commit

---
+++ stellar_rust_sdk/src/trade_aggregations/mod.rs
@@ -0,0 +1,15 @@
+// TODO: Documentation
+pub mod trade_aggregations_request;
+
+// TODO: Documentation
+static TRADES_PATH: &str = "trades";
+
+// TODO: Documentation
+pub mod prelude {
+    pub use super::trade_aggregations_request::*;
+}
+
+#[cfg(test)]
+pub mod test {
+    // TODO: Write tests
+}
