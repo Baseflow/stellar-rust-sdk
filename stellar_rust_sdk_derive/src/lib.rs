be5839c05713a08d9474306c68c43b1213f901bb
Kevin Pease <kevin@baseflow.com>
2024-08-20 14:06:00 +0200

    Add documentation

--- stellar_rust_sdk_derive/src/lib.rs
+++ stellar_rust_sdk_derive/src/lib.rs
@@ -3,11 +3,35 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemStruct, Fields, Field};
 
+/// The procedural attribute macro to add pagination functionality to request structs.
+///
+/// This macro automatically injects pagination-related fields and methods into a struct
+/// to facilitate paginated API requests. Specifically, it adds three optional fields
+/// and three methods:
+///
+/// - `cursor`: An `Option<u32>` field that represents the pagination cursor. The cursor
+///   is used to track the current position in a paginated dataset. The `set_cursor` method
+///   allows setting this field, with a validation that ensures the cursor is greater than
+///   or equal to 1.
+///
+/// - `limit`: An `Option<u8>` field that specifies the maximum number of items to retrieve
+///   in a single page. The `set_limit` method allows setting this field, ensuring that the
+///   limit is within a valid range (between 1 and 200).
+///
+/// - `order`: An `Option<Order>` field that defines the sort order of the paginated results.
+///   The `set_order` method allows setting this field without additional validation, as the
+///   sort order is context-dependent.
+///
+/// # Usage
+///
+/// Apply the `#[pagination]` attribute to a struct to automatically add pagination
+/// functionality.
+///
 #[proc_macro_attribute]
 pub fn pagination(_attr: TokenStream, input: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(input as ItemStruct);
 
-    // create req'd fields
+    // Create required fields to be added to the struct.
     let cursor_field: Field = syn::parse_quote! {
         pub cursor: Option<u32>
     };
@@ -18,6 +42,7 @@ pub fn pagination(_attr: TokenStream, input: TokenStream) -> TokenStream {
         pub order: Option<Order>
     };
 
+    // Add the fields to the struct.
     if let Fields::Named(ref mut fields) = input.fields {
         fields.named.push(cursor_field);
         fields.named.push(limit_field);
@@ -25,7 +50,11 @@ pub fn pagination(_attr: TokenStream, input: TokenStream) -> TokenStream {
     }
 
     let struct_name = &input.ident;
+
+    // Split the generics into implementation, type, and where clause parts, so that the macro supports generic structs.
     let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
+
+    // Add methods to the struct.
     let expanded = quote! {
         #input
         impl #impl_generics #struct_name #type_generics #where_clause {
