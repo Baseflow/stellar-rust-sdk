11bb62cac26311df3f124016285318481e16ac76
Kevin Pease <kevin@baseflow.com>
2024-09-13 14:12:54 +0200

    Improve  methods

--- stellar_rust_sdk/src/order_book/details_request.rs
+++ stellar_rust_sdk/src/order_book/details_request.rs
@@ -89,41 +89,50 @@ impl DetailsRequest<SellingAsset, NoBuyingAsset> {
 
 impl Request for DetailsRequest<SellingAsset, BuyingAsset> {
     fn get_query_parameters(&self) -> String {
-        let mut query: Vec<String> = Vec::new();
-
-        match &self.selling_asset.0 {
-            AssetType::Native => {
-                query.push(format!("selling_asset_type=native"));
+        vec![&self.selling_asset.0, &self.buying_asset.0]
+        .iter()
+        .enumerate()
+        .fold(Vec::new(), |mut parameters, (i, asset)| {
+            let asset_type_prefix = if i == 0 {
+                "selling_asset_type="
             }
-            AssetType::Alphanumeric4(asset) => {
-                query.push(format!("selling_asset_type=credit_alphanum4"));
-                query.push(format!("&selling_asset_code={}", asset.asset_code));
-                query.push(format!("&selling_asset_issuer={}", asset.asset_issuer));
+            // no `&` for `base_asset_type`, as the query begins with `?`
+            else {
+                "&buying_asset_type="
+            };
+            match asset {
+                AssetType::Native => parameters.push(format!("{}native", asset_type_prefix)),
+                AssetType::Alphanumeric4(asset_data)
+                | AssetType::Alphanumeric12(asset_data) => {
+                    let asset_type = match asset {
+                        AssetType::Alphanumeric4(_) => "credit_alphanum4",
+                        AssetType::Alphanumeric12(_) => "credit_alphanum12",
+                        _ => "", // should not be reached
+                    };
+                    let asset_issuer_prefix = if i == 0 {
+                        "&selling_asset_issuer="
+                    } else {
+                        "&buying_asset_issuer="
+                    };
+                    let asset_code_prefix = if i == 0 {
+                        "&selling_asset_code="
+                    } else {
+                        "&buying_asset_code="
+                    };
+                    parameters.push(format!(
+                        "{}{}{}{}{}{}",
+                        asset_type_prefix,
+                        asset_type,
+                        asset_code_prefix,
+                        asset_data.asset_code,
+                        asset_issuer_prefix,
+                        asset_data.asset_issuer
+                    ));
+                }
             }
-            AssetType::Alphanumeric12(asset) => {
-                query.push(format!("selling_asset_type=credit_alphanum12"));
-                query.push(format!("&selling_asset_code={}", asset.asset_code));
-                query.push(format!("&selling_asset_issuer={}", asset.asset_issuer));
-            }
-        }
-
-        match &self.buying_asset.0 {
-            AssetType::Native => {
-                query.push(format!("&buying_asset_type=native"));
-            }
-            AssetType::Alphanumeric4(asset) => {
-                query.push(format!("&buying_asset_type=credit_alphanum4"));
-                query.push(format!("&buying_asset_code={}", asset.asset_code));
-                query.push(format!("&buying_asset_issuer={}", asset.asset_issuer));
-            }
-            AssetType::Alphanumeric12(asset) => {
-                query.push(format!("&buying_asset_type=credit_alphanum12"));
-                query.push(format!("&buying_asset_code={}", asset.asset_code));
-                query.push(format!("&buying_asset_issuer={}", asset.asset_issuer));
-            }
-        }
-
-        query.join("")
+            parameters
+        })
+        .join("")
     }
 
     fn build_url(&self, base_url: &str) -> String {
